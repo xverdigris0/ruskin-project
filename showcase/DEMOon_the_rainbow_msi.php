d10ddc1cadbcde59afac04a92b4440df8011e94c
xverdigris0 <johntylerwilson@gmail.com>
2016-10-23 18:14:13 -0500

    navbar

--- showcase/DEMOon_the_rainbow_msi.php
+++ showcase/DEMOon_the_rainbow_msi.php
@@ -366,60 +366,14 @@ $(document).ready(function () {
       }
   }
   
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });  
-});
+ $.fn.popover.Constructor
+ 
+ 
 </script>
 </head>
 <body>
-<div id="wrapper">
-    <div class="overlay"></div>
-    
-        <!-- Sidebar -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
-            <ul class="nav sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                       Brand
-                    </a>
-                </li>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Events</a>
-                </li>
-                <li>
-                    <a href="#">Team</a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Works <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li class="dropdown-header">Dropdown heading</li>
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-                <li>
-                    <a href="https://twitter.com/maridlcrmn">Follow me</a>
-                </li>
-            </ul>
-        </nav>
-<div class="container">    
-<div id="page-content-wrapper"> 
+
+<div class="container">   
 <div id="main">
   <div id="navbar-tab">
 	<img id="toggle-navbar" src="images/navbar/tab_ro.jpg" alt="Show/Hide NavBar Button" title="Show/Hide the Navbar" width="20" height="10" />
@@ -577,7 +531,5 @@ $(document).ready(function () {
   </div>
 </div>
 </div>    
-</div>
-</div>
 </body>
 </html>
